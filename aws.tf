1e5d2378df979fe4dd5145edf954cd7112f921fb
James Woolfenden <James.Woolfenden@gmail.com>
2020-01-23 10:51:34 +0000

    wip

--- aws.tf
+++ aws.tf
@@ -3,8 +3,3 @@ data "aws_organizations_organization" "example" {}
 output "account_ids" {
   value = data.aws_organizations_organization.example.accounts[*].id
 }
-
-provider "aws" {
-    version="2.45"
-    region="eu-west-1"
-}
